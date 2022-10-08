d6296a306652b3075d40f38636bc9d6d30ceff12
KOL305 <kadonliang@gmail.com>
2022-10-08 13:49:49 -0700

    fixed flask routes + added base.html

--- app.py
+++ app.py
@@ -29,29 +29,33 @@ def home():
         return render_template('home.html')
 
 @app.route('/draw', methods=['GET','POST'])
-def home():
+def draw():
     if request.method == 'GET':     
         return render_template('home.html')
     elif request.method == 'POST':
         return render_template('home.html')
 
 @app.route('/calculator', methods=['GET','POST'])
-def home():
+def calculator():
     if request.method == 'GET':     
         return render_template('home.html')
     elif request.method == 'POST':
         return render_template('home.html')
 
 @app.route('/tracker', methods=['GET','POST'])
-def home():
+def tracker():
     if request.method == 'GET':     
         return render_template('home.html')
     elif request.method == 'POST':
         return render_template('home.html')
 
 @app.route('/dashboard', methods=['GET','POST'])
-def home():
+def dashboard():
     if request.method == 'GET':     
         return render_template('home.html')
     elif request.method == 'POST':
         return render_template('home.html')
+
+if __name__ == "__main__":
+    app.config['SECRET_KEY'] = '123qwi34iWge9era89F1393h3gwJ0q3'
+    app.run(debug=True)
